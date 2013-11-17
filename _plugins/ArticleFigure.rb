b2025203956d48f77eb9d50ec371151fc3587a6b
redraiment <redraiment@gmail.com>
2013-11-17 18:38:13 +0800

    插入图片是可指定align

--- _plugins/ArticleFigure.rb
+++ _plugins/ArticleFigure.rb
@@ -5,12 +5,18 @@ module IKnowledge
   class ArticleFigureTag < Liquid::Tag
     def initialize(tag, text, tokens)
       super
-      @name = text;
+      value = text.split
+      @name = value[0];
+      @align = value.length > 1? value[1]: ''
     end
 
     def render(context)
       page = context.registers[:page]
-      "<img src=\"/resource#{page['url']}#@name\" />"
+      if @align == ''
+        "<img src=\"/resource#{page['url']}#@name\" />"
+      else
+        "<img align=\"#@align\" src=\"/resource#{page['url']}#@name\" />"
+      end
     end
   end
 end

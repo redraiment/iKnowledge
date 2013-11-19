a849e215de34df46ecedce00b84fd2cde70742d8
redraiment <redraiment@gmail.com>
2013-11-19 19:30:32 +0800

    处理用户未安装Grit库的情况，用户为初始化_posts时亦不出错

--- _plugins/GitLog.rb
+++ _plugins/GitLog.rb
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-require 'grit'
-
 # iKnowledge特色功能之一
 # 每篇文章末尾提供Git提交的记录，即展示修改历史
 # 很多人从来不会回头看自己写的文章/博客
@@ -10,11 +8,27 @@ module IKnowledge
   # 按照类型归档
   class PageChangeLogGenerator < Jekyll::Generator
     def generate(site)
+      begin
+        require 'grit'
+      rescue LoadError
+        # 如果用户为安装grit
+        # 并不强制用户使用修改历史插件
+        puts "如果您希望使用“修改历史”插件，请安装grit库"
+        return
+      end
+
       pwd = Dir.pwd
 
       workdir = File.join(site.source, '_posts')
       Dir.chdir(workdir)
-      git = Grit::Repo.new('.')
+      begin
+        git = Grit::Repo.new('.')
+      rescue
+        puts "如果您希望使用“修改历史”插件，请在_posts目录下创建git仓库"
+        puts "  cd _posts"
+        puts "  git init"
+        return
+      end
 
       site.posts.each do |post|
         post.data['logs'] = git.log(File.basename(post.path)).map do |log|
